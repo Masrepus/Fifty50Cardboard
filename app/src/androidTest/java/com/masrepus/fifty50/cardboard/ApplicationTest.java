303cbcb8c3a247ec68c95d61b3c8cbdd8cc6d521
Samuel <masrepus97@gmail.com>
2016-01-18 15:38:14 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/masrepus/fifty50/cardboard/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.masrepus.fifty50.cardboard;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
