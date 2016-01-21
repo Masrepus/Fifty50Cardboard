f60f5969f747a99f29f26cdceaffd7d489e5117c
Samuel <masrepus97@gmail.com>
2016-01-21 18:15:47 +0100

    implemented gesture detector

--- app/src/main/java/com/masrepus/fifty50/cardboard/MjpegActivity.java
+++ app/src/main/java/com/masrepus/fifty50/cardboard/MjpegActivity.java
@@ -1,47 +1,48 @@
 package com.masrepus.fifty50.cardboard;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import com.google.vrtoolkit.cardboard.CardboardActivity;
-import com.google.vrtoolkit.cardboard.CardboardView;
-import com.google.vrtoolkit.cardboard.Eye;
-import com.google.vrtoolkit.cardboard.HeadTransform;
-import com.google.vrtoolkit.cardboard.Viewport;
-import com.google.vrtoolkit.cardboard.sensors.HeadTracker;
-import com.masrepus.fifty50.cardboard.MjpegInputStream;
-import com.masrepus.fifty50.cardboard.MjpegView;
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
-import javax.microedition.khronos.egl.EGLConfig;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.sensors.HeadTracker;
 
-public class MjpegActivity extends CardboardActivity implements CardboardView.StereoRenderer {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MjpegActivity extends CardboardActivity {
     private static final String TAG = "MjpegActivity";
 
     private MjpegView mv;
-    private HeadTransform headTransform;
     private Direction direction = Direction.STRAIGHT;
     private Mode mode = Mode.BRAKE;
     private boolean running;
+    private String url;
+    private HeadTracker tracker;
+    private ArrayList<String> trackerData = new ArrayList<>();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //sample public cam
+        //car cam
         String URL = "http://192.168.178.31:8080/?action=stream";
+
+        SharedPreferences pref = getSharedPreferences("prefs", 0);
+        url = pref.getString("url", "192.168.42.1");
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -51,6 +52,9 @@ public class MjpegActivity extends CardboardActivity implements CardboardView.St
         setContentView(mv);
 
         running = true;
+
+        tracker = HeadTracker.createFromContext(this);
+        tracker.startTracking();
 
         new GestureDetector().start();
         new DoRead().execute(URL);
@@ -67,42 +71,12 @@ public class MjpegActivity extends CardboardActivity implements CardboardView.St
         if (hasFocus) {
             mv.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
-    }
-
-    @Override
-    public void onNewFrame(HeadTransform headTransform) {
-        this.headTransform = headTransform;
-    }
-
-    @Override
-    public void onDrawEye(Eye eye) {
-
-    }
-
-    @Override
-    public void onFinishFrame(Viewport viewport) {
-
-    }
-
-    @Override
-    public void onSurfaceChanged(int i, int i1) {
-
-    }
-
-    @Override
-    public void onSurfaceCreated(EGLConfig eglConfig) {
-
-    }
-
-    @Override
-    public void onRendererShutdown() {
-
     }
 
     public Direction getDirection() {
@@ -113,15 +87,77 @@ public class MjpegActivity extends CardboardActivity implements CardboardView.St
         return mode;
     }
 
+    private void left() {
+
+    }
+
+    private void right() {
+
+    }
+
+    private void testSensor() {
+
+        int count = 0;
+
+        while (count <= 100) {
+            Log.d(TAG, String.valueOf(count));
+            float[] headview = new float[16];
+            tracker.getLastHeadView(headview, 0);
+            trackerData.add(Arrays.toString(headview).replace("[", "").replace("]", ""));
+            count++;
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        saveFile();
+    }
+
+    private void saveFile() {
+        String fnm = "sensor-oben-unten-5.csv";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(fnm, Context.MODE_PRIVATE);
+
+            for (int i = 0; i < trackerData.size(); i++) {
+                outputStream.write((trackerData.get(i) + "\n").getBytes());
+            }
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "finished");
+    }
+
+    private void straight() {
+
+    }
+
+    private void forward() {
+
+    }
+
+    private void backward() {
+
+    }
+
+    private void brake() {
+
+    }
+
     public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
         protected MjpegInputStream doInBackground(String... url) {
             HttpResponse res = null;
             DefaultHttpClient httpclient = new DefaultHttpClient();
             Log.d(TAG, "1. Sending http request");
             try {
-                res = httpclient.execute(new HttpGet(new URI("http", null, "192.168.178.31", 8080, "/", "action=stream", "anchor")));
+                res = httpclient.execute(new HttpGet(new URI("http", null, MjpegActivity.this.url, 8080, "/", "action=stream", "anchor")));
                 Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
-                if(res.getStatusLine().getStatusCode()==401){
+                if (res.getStatusLine().getStatusCode() == 401) {
                     //You must turn off camera User Access Control before this will work
                     return null;
                 }
@@ -154,68 +190,57 @@ public class MjpegActivity extends CardboardActivity implements CardboardView.St
         @Override
         public void run() {
 
-            while (running) {
+            //check the current head transform and send the appropriate command
+            if (tracker != null) {
 
-                //check the current head transform and send the appropriate command
-                if (headTransform != null) {
+                float[] headView = new float[3];
+                tracker.getLastHeadView(headView, 0);
 
-                    float[] eulerAngles = new float[3];
-                    headTransform.getEulerAngles(eulerAngles, 0);
+                //if looking right/left, send right/left command, etc.
+                if (headView[8] > 0.2) direction = Direction.RIGHT;
+                else if (headView[8] < -0.2) direction = Direction.LEFT;
+                else direction = Direction.STRAIGHT;
 
-                    //if roll is right, send "right" command, etc.
-                    if (eulerAngles[2] > 0.17) direction = Direction.RIGHT;
-                    else if (eulerAngles[2] < -0.17) direction = Direction.LEFT;
-                    else direction = Direction.STRAIGHT;
+                //looking up or down?
+                if (headView[9] < -0.2) mode = Mode.BACKWARD;
+                else if (headView[9] > 0.2) mode = Mode.FORWARD;
+                else mode = Mode.BRAKE;
 
-                    //check pitch angle
-                    if (eulerAngles[0] > 0.17) mode = Mode.BACKWARD;
-                    else if (eulerAngles[0] < -0.17) mode = Mode.FORWARD;
-                    else mode = Mode.BRAKE;
-
-                    //now send command
-                    switch (direction) {
-                        case LEFT: left();
-                            break;
-                        case RIGHT: right();
-                            break;
-                        case STRAIGHT: straight();
-                            break;
-                    }
-
-                    switch (mode) {
-                        case FORWARD: forward();
-                            break;
-                        case BACKWARD: backward();
-                            break;
-                        case BRAKE: brake();
-                            break;
-                    }
+                //now send command
+                switch (direction) {
+                    case LEFT:
+                        left();
+                        break;
+                    case RIGHT:
+                        right();
+                        break;
+                    case STRAIGHT:
+                        straight();
+                        break;
                 }
+
+                Log.d(TAG, direction.name());
+
+                switch (mode) {
+                    case FORWARD:
+                        forward();
+                        break;
+                    case BACKWARD:
+                        backward();
+                        break;
+                    case BRAKE:
+                        brake();
+                        break;
+                }
+
+                Log.d(TAG, mode.name());
+            } else Log.d(TAG, "tracker == null");
+
+            try {
+                Thread.sleep(100, 0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-    }
-
-    private void left() {
-
-    }
-
-    private void right() {
-
-    }
-
-    private void straight() {
-
-    }
-
-    private void forward() {
-
-    }
-
-    private void backward() {
-
-    }
-
-    private void brake() {
-
     }
 }
